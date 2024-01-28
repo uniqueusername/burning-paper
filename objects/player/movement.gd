1d3aec4902424538d3ed09e199497557664e5a8d
qubitly <32994163+qubitly@users.noreply.github.com>
2024-01-27 18:54:34 -0800

    guy has arrived
    added guy and removed incorrect wall segment

--- objects/player/movement.gd
+++ objects/player/movement.gd
@@ -45,9 +45,12 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+<<<<<<< Updated upstream:objects/player/movement.gd
 	
 		
 	
+=======
+>>>>>>> Stashed changes:burning paper/movement.gd
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
