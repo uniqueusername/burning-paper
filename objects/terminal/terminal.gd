dee0f53ec34b2e0520284bf55b57b20da2bf3973
ary <aryan@jha.io>
2024-01-27 20:46:45 -0800

    added terminal outline on hover

--- objects/terminal/terminal.gd
+++ objects/terminal/terminal.gd
@@ -1,4 +1,14 @@
-extends MeshInstance3D
+extends CSGBox3D
 
 func _ready():
 	add_to_group("terminals")
+
+func _on_highlight():
+	$outline.visible = true
+
+func _on_unhighlight():
+	$outline.visible = false
+
+func connect_highlight_signals(player: Node):
+	player.highlight.connect(_on_highlight)
+	player.unhighlight.connect(_on_unhighlight)
